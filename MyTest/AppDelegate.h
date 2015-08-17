4de086b7c6d663548e1146518de7684966cc0347
Mac <mac@MacdeMac-mini.local>
2015-08-17 14:07:23 +0800

    Initial Commit

---
+++ MyTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyTest
+//
+//  Created by Mac on 15/8/17.
+//  Copyright (c) 2015年 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyTest
+//
+//  Created by Mac on 15/8/17.
+//  Copyright (c) 2015年 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
